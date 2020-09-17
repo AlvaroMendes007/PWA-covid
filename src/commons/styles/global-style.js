51e2ccc7e5d83bbe8dc245d77b0dd921de7704c8
Alvaro Fontes Costa Mendes <alvaro.mendes@grupoverreschi.com.br>
2020-09-17 11:39:56 -0300

    add Style

---
+++ src/commons/styles/global-style.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+import CovidImg from '../../assets/images/covid.jpg'
+
+const globalStyle = createGlobalStyle`
+    *{
+        outline: none;
+        box-sizing: border-box;
+    }
+
+    body{
+        line-height: normal;
+    }
+
+    html, body{
+        width: 100%;
+        min-height: 100%;
+        display: flex;
+        padding: 0;
+        margin: 0;
+    }
+
+    #root{
+        background: url(${CovidImg});
+        height: 100%;
+        width: 100%;
+        background-size: cover;
+        background-position: center center;
+    }
+`
+export default globalStyle;
