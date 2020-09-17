25562161eb67d2e280d89f2d8fe67da2877c8ecb
Alvaro Fontes Costa Mendes <alvaro.mendes@grupoverreschi.com.br>
2020-09-17 12:12:23 -0300

    finish project

--- src/commons/styles/global-style.js
+++ src/commons/styles/global-style.js
@@ -2,41 +2,43 @@ import { createGlobalStyle } from 'styled-components'
 import CovidImg from '../../assets/images/covid.jpg'
 
 const globalStyle = createGlobalStyle`
-    *{
-        outline: none;
-        box-sizing: border-box;
-    }
+  * {
+    outline: none;
+    box-sizing: border-box;
+  }
 
-    body{
-        line-height: normal;
-    }
+  body {
+    line-height: normal;
+  }
 
-    html, body{
-        width: 100%;
-        min-height: 100%;
-        display: flex;
-        padding: 0;
-        margin: 0;
-    }
+  html, body {
+    width: 100%;
+    min-height: 100%;
+    display: flex;
+    padding: 0;
+    margin: 0;
+  }
 
-    #root{
-        background: url(${CovidImg});
-        height: 100%;
-        width: 100%;
-        background-size: cover;
-        background-position: center center;
-    }
+  #root {
+    background: url(${CovidImg});
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    background-position: center center;
+  }
 
-    .mb-2 {
-        margin-bottom: 16px;
-    }
+  .mb-2 {
+    margin-bottom: 16px;
+  }
 
-    .pt-2 {
-        padding-top: 16px;
-    }
+  .pt-2 {
+    padding-top: 16px;
+  }
 
-    .cursor {
-        cursor: pointer;
-    }
+  .cursor {
+    cursor: pointer;
+  }
+
 `
-export default globalStyle;
+
+export default globalStyle
--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,12 +1,43 @@
-import React, { memo } from 'react'
+import React, { memo, useState, useCallback, useEffect } from 'react'
+import Api from '../../api'
+import Board from './components/Board'
+import Panel from './components/Panel'
+import { ContainerStyled } from './style'
 
 function Main() {
+  const [data, setData] = useState({})
+  const [country, setCountry] = useState('brazil')
+  const updateAt = new Date().toLocaleString()
 
-    return (
-        <div>
-            Teste
-        </div>
-    )
+  const getCovidData = useCallback((country) => {
+    Api.getCountry(country)
+      .then(data => setData(data))
+  }, [])
+
+  useEffect(() => {    
+    getCovidData(country)
+  }, [getCovidData, country])
+
+  const handleChange = ({ target }) => {
+    const country = target.value
+    setCountry(country)
+  }
+
+  return (
+    <ContainerStyled>
+      <div className="mb-2">
+        <Panel
+          data={data}
+          updateAt={updateAt}
+          onChange={handleChange}
+          country={country}
+          getCovidData={getCovidData}
+        />
+      </div>
+      <Board data={data} />
+      
+    </ContainerStyled>
+  )
 }
 
 export default memo(Main)
