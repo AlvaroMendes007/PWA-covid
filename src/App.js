df594d7385a598407ed06678d1cc22396980344b
Alvaro Fontes Costa Mendes <alvaro.mendes@grupoverreschi.com.br>
2020-09-17 11:49:33 -0300

    configure api

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { StylesProvider } from '@material-ui/styles'
 import { CssBaseline } from '@material-ui/core'
 import GlobalStyle from './commons/styles/global-style'
+import Main from './containers/Main'
 
 function App() {
   return (
     <StylesProvider injectFirst>
       <CssBaseline />
       <GlobalStyle />
-      <div>
-        tete
-      </div>
+      <Main />
     </StylesProvider>
   );
 }
